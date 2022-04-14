35a0239bca82c83fad7c9a2bbb6025e97afd9081
swapnil <sopiswarrinku77@gmail.com>
2022-04-14 13:20:25 -0700

    part time wages

---
+++ Part_Time_employee_wage.java
@@ -0,0 +1,19 @@
+public class Part_Time_employee_wage {
+}
+    public static void main(String[] args) {
+        // constants
+        int IS_FULL_TIME = 1;
+        int IS_PART_TIME = 2;
+        int EMP_RATE_PER_HOUR = 20;
+        // variables
+        int empHrs = 0;
+        int empWage = 0;
+        // computation
+        double empCheck = Math.floor(math.random() * 10) % 3;
+        if (empcheck == IS_FULL-TIME )
+            empHrs = 4;
+        empWage = emoHrs * EMP_RATE_PER_HOUR;
+        System.out.println("Emp Wage : " + empWage);
+
+
+    }
